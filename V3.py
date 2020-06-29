d86e47a80e9e6c7e501ae1b5468025d7e9058608
Amaury Semery <amaury.semery@laposte.net>
2020-06-29 16:45:56 +0200

    Version réussie

--- V3.py
+++ V3.py
@@ -10,16 +10,30 @@ def lancerDeDes(nbDes,nbFaces):
         d = lancerUnDe(nbFaces)
         listeDesDes.append(d)
     return listeDesDes
+ 
+def resultat(lancer):
+    s = 0
+    reu = 0
+    for i in lancer:
+        if i > 5:
+            s=s+1
+            reu=reu+1
+        if i > 1 and i < 6:
+            s=s
+        if i == 1:
+            s=s-1
+        if s < 0:
+            r="Echec critique"
+        if s == 0:
+            r="Echec"
+        if s > 0:
+            r="Valeur de la réussite", reu
+    print(r)
         
 nbDes = int(input("Saisissez le nombre de dés : "))
 nbFaces = int(input("Saisissez le nombre de faces pour chaque dé : "))
-lancer = print("Le résultat du lancer donne : ",lancerDeDes(nbDes,nbFaces))
+l=lancerDeDes(nbDes,nbFaces)
 
-def resultat(lancer):
-    for i in lancerDeDes(nbDes,nbFaces):
-        if i > 5:
-            print("Réussite")
-        else:
-            print("Echec")
+print("Le résultat du lancer donne : ", l)
 
-print(resultat(lancer))
+print(resultat(l))
